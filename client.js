fb24864516429446ae8f9f81e67d59cddcb78a9a
Bode Falade <bode.falade@outlook.com>
2018-12-14 17:42:39 -0600

    input field entry is collected and stored in an array of Employee class

--- client.js
+++ client.js
@@ -1,6 +1,28 @@
+//Employee class template
+class Employee {
+    constructor(firstNameIn, lastNameIn, idIn, titleIn, salaryIn) {
+        this.first = firstNameIn;
+        this.last = lastNameIn;
+        this.id = idIn;
+        this.title = titleIn;
+        this.salary = salaryIn;
+    }//end constructor
+}// end of Employee class
+
+let employees = [];
+
 console.log('JS loaded');
 
 $(document).ready(function(){
     console.log('JQ loaded');
+
+    $('#submit').on('click', getInput);
     
 });
+
+function getInput() {
+    //get inputs
+    let inputsGet = new Employee($('#firstNameIn').val(), $('#lastNameIn').val(), 
+                    $('#idIn').val(), $('#titleIn').val(), $('#salary').val());
+    employees.push(inputsGet);
+}
